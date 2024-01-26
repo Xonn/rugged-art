2680d20c05cb17eed277f975b17c5707c84310fe
Bastien <bsp95@hotmail.fr>
2024-01-26 10:31:31 +0100

    Add AlexBrush font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -21,6 +21,7 @@ module.exports = {
         'bertioga': ['Bertioga'],
         'fraunces': ['Fraunces'],
         'baskervville': ['Baskervville'],
+        'alex-brush': ['AlexBrush'],
       },
       colors: {
         border: "hsl(var(--border))",
