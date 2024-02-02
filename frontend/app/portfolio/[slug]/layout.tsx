a6bb3c54f0c22a692d15c6e5d5c881ef4ce651d2
Bastien <bsp95@hotmail.fr>
2024-02-02 11:32:07 +0100

    Remove article categories in portfolio layout

--- frontend/app/portfolio/[slug]/layout.tsx
+++ frontend/app/portfolio/[slug]/layout.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 
-import ArticleSelect from "../../components/ArticleSelect";
-import { fetchAPI } from "../../utils/fetch-api";
-
 export default async function LayoutRoute({
   params,
   children,
@@ -19,27 +16,3 @@ export default async function LayoutRoute({
     </div>
   );
 }
-
-// export async function generateStaticParams() {
-//   const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
-//   const path = `/projects`;
-//   const options = { headers: { Authorization: `Bearer ${token}` } };
-//   const articleResponse = await fetchAPI(
-//     path,
-//     {
-//       populate: ["categories"],
-//     },
-//     options
-//   );
-
-//   return articleResponse.data.map(
-//     (article: {
-//       attributes: {
-//         slug: string;
-//         category: {
-//           slug: string;
-//         };
-//       };
-//     }) => ({ slug: article.attributes.slug, category: article.attributes.slug })
-//   );
-// }
