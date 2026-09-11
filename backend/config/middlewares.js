8864c4430d450a27e5ef85aa3834e5a90bbcca8f
Bastien <bsp95@hotmail.fr>
2026-09-11 11:00:08 +0200

    fix(cors): use env array for CORS origin configuration

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,13 +1,13 @@
-module.exports = [
+module.exports = ({ env }) => [
   'strapi::errors',
   'strapi::security',
   {
     name: 'strapi::cors',
     config: {
-      origin: [
+      origin: env.array('CORS_ORIGIN', [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
-      ],
+      ]),
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       headers: [
         'Content-Type',
